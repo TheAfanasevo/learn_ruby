5ac089560c4bf0d5cbd3b818cfc230b8b398854a
Mustafa Kursun <kursun.must@gmail.com>
2020-03-21 14:36:28 +0700

    Calculator methods created

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def multiply(*args)
+    args.inject(:*)
+end
+
+def sum array
+    array.sum
+end
+
+def power(a, b)
+    a**b
+end
+
+def factorial num
+    if num == 0
+        1
+    else
+        (1..num).inject(:*)
+    end
+end
