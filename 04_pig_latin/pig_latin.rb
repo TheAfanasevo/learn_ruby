0fdde140da1b10878627c8e1f9c7ae102cf9615e
Mustafa Kursun <kursun.must@gmail.com>
2020-03-22 16:46:28 +0700

    Pig latin exercise completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+
+# for more about regular expressins: https://blog.benroux.me/understanding-regular-expressions-2-groups-and-captures/
+# \b indicates beginning or end of words
+# \s is added for many words (with white space)
+
+# first group
+# \b[aeiou]\w* a vowel as the first character which is followed by others or
+# \bqu 'qu' as the first two characters or
+# \b[^\saeiou]qu 'qu' as the first two characters but not preceded by a space or vowel or
+# \b[^\saeiou] consonant as the first character
+# \b[^\saeiou]{1,3} consonants as the first three characters or
+
+# second group
+# \w* the whole group of characters after the first group
+
+def translate text
+    text.gsub!(/(\b[aeiou]\w*|\bqu|\b[^\saeiou]qu|\b[^\saeiou]{1,3}|\b[^\saeiou])(\w*)/, '\\2\\1ay')
+end
